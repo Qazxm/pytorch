79220c6ed0c88c1f87d0577dad07e6cd7666b3da
Qazxm <seyeong025@gmail.com>
2024-08-22 18:13:07 +0900

    디자인 수정

--- app/src/main/java/com/example/pytorch/network/RetrofitClient.kt
+++ app/src/main/java/com/example/pytorch/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://0fad-211-196-103-173.ngrok-free.app/" // 실제 Flask 서버 주소로 교체
+    private const val BASE_URL = "https://4e50-211-196-103-173.ngrok-free.app/" // 실제 Flask 서버 주소로 교체
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
